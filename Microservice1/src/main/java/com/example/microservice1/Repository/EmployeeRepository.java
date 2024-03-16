c0d90a1debb1207a5e04a12764742df54f872aec
NOUR-BEN-ALI <98612703+NOUR-BEN-ALI@users.noreply.github.com>
2024-03-16 11:56:25 +0100

    Delete Microservice1 directory

--- Microservice1/src/main/java/com/example/microservice1/Repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.microservice1.Repository;
-
-import com.example.microservice1.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> { }
