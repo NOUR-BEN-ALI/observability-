e8b983f7796f11836440cc0a7d7ac2ffaf246fc6
NOUR-BEN-ALI <98612703+NOUR-BEN-ALI@users.noreply.github.com>
2024-03-16 11:56:53 +0100

    Delete Microservice3 directory

--- Microservice3/src/main/java/com/example/microservice3/Repository/RecruteurRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.microservice3.Repository;
-
-import com.example.microservice3.entity.Recruteur;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecruteurRepository extends JpaRepository<Recruteur, Long> { }
